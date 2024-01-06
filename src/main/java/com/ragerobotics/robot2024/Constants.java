897fe0d23e485f80028159971f9b1ef1d5c2cb31
Caleb Heydon <caleb@caleb.engineer>
2024-01-06 14:42:27 -0500

    Added to SwerveModule implementation

---
+++ src/main/java/com/ragerobotics/robot2024/Constants.java
@@ -0,0 +1,6 @@
+package com.ragerobotics.robot2024;
+
+public class Constants {
+    public static final double kWheelRadius = 0.15; // meters
+    public static final double kEncoderTicksPerWheelRotation = 2048.0 * 6.75;
+}
--- src/main/java/com/ragerobotics/robot2024/SwerveModule.java
+++ src/main/java/com/ragerobotics/robot2024/SwerveModule.java
@@ -1,7 +1,12 @@
 package com.ragerobotics.robot2024;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 public class SwerveModule {
     private TalonFX m_driveMotor;
@@ -10,5 +15,31 @@ public class SwerveModule {
     public SwerveModule(TalonFX driveMotor, TalonSRX steeringMotor) {
         m_driveMotor = driveMotor;
         m_steeringMotor = steeringMotor;
+    }
+
+    public SwerveModuleState getState() {
+        double distance = m_driveMotor.getSelectedSensorVelocity() / Constants.kEncoderTicksPerWheelRotation
+                / (m_driveMotor.getStatusFramePeriod(0) / 1000.0) * 2 * Math.PI * Constants.kWheelRadius;
+        double angle = 0;
+
+        return new SwerveModuleState(distance, new Rotation2d(angle));
+    }
+
+    public SwerveModulePosition getPosition() {
+        double distance = m_driveMotor.getSelectedSensorPosition() / Constants.kEncoderTicksPerWheelRotation * 2
+                * Math.PI * Constants.kWheelRadius;
+        double angle = 0;
+
+        return new SwerveModulePosition(distance, new Rotation2d(angle));
+    }
+
+    public void drive(SwerveModuleState setpoint) {
+        double angle = 0;
+        Rotation2d rot = new Rotation2d(angle);
+
+        SwerveModuleState state = SwerveModuleState.optimize(setpoint, rot);
+        m_driveMotor.set(ControlMode.Velocity, state.speedMetersPerSecond
+                * (1000.0 / m_driveMotor.getStatusFramePeriod(0)) * Constants.kEncoderTicksPerWheelRotation);
+        m_steeringMotor.set(ControlMode.Position, state.angle.getDegrees());
     }
 }
