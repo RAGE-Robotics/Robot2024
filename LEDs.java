68099999e4b5721a1216586e0db206bba2b4be90
RobertFrazier265 <146474584+RobertFrazier265@users.noreply.github.com>
2024-02-20 15:05:57 -0500

    Add files via upload

---
+++ LEDs.java
@@ -0,0 +1,68 @@
+package com.ragerobotics.robot2024.systems;
+
+import java.util.Optional;
+
+import com.ragerobotics.robot2024.Robot.Mode;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
+public class LEDs {
+    private static LEDs instance;
+
+    public static LEDs getInstance() {
+        if (instance == null) {
+            instance = new LEDs();
+        }
+
+        return instance;
+
+    }
+
+    AddressableLED m_led = new AddressableLED(0);
+    AddressableLEDBuffer m_ledBuffer = new AddressableLEDBuffer(300);
+
+    int m_rainbowFirstPixelHue; 
+    
+    Optional<Alliance> alliance = DriverStation.getAlliance();
+     
+    public LEDs() {
+        m_led.setLength(m_ledBuffer.getLength());
+        m_led.setData(m_ledBuffer);
+        m_led.start();
+    }
+
+    public void allianceColor() {
+
+        if (alliance.get() == Alliance.Red) {
+        
+        for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+            m_ledBuffer.setRGB(i, 255, 0, 0);
+            }
+        }
+
+        else {
+           for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+            m_ledBuffer.setRGB(i, 0, 0, 255);
+            } 
+        }
+
+        m_led.setData(m_ledBuffer);
+        
+     }
+
+     public void rainbowColors() {
+        for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+            final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
+
+            m_ledBuffer.setHSV(i, hue, 255, 128);
+        }
+
+        m_rainbowFirstPixelHue += 3;
+
+        m_rainbowFirstPixelHue %= 180;
+
+     }
+}
