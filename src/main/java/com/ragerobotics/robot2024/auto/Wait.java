be3a2123f2f9d8067e24c0899256351b087446ee
Caleb Heydon <caleb@caleb.engineer>
2024-02-24 15:09:58 -0500

    Added Wait.java

---
+++ src/main/java/com/ragerobotics/robot2024/auto/Wait.java
@@ -0,0 +1,27 @@
+package com.ragerobotics.robot2024.auto;
+
+public class Wait implements ITask {
+    private double m_delay = 0;
+    private double m_startTime = -1;
+    private double m_lastTime = 0;
+
+    public Wait(double delay) {
+        m_delay = delay;
+    }
+
+    public void onStart(double timestamp) {
+        m_startTime = timestamp;
+    }
+
+    public void onUpdate(double timestamp) {
+        m_lastTime = timestamp;
+    }
+
+    public boolean isDone() {
+        return m_lastTime > m_startTime + m_delay;
+    }
+
+    public void onStop() {
+
+    }
+}
