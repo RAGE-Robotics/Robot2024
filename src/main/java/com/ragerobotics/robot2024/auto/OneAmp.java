566d3bd64482dd4e882e5bf482b9eefa4ece66ca
Caleb Heydon <caleb@caleb.engineer>
2024-03-02 10:24:12 -0500

    Updated autonomous

--- src/main/java/com/ragerobotics/robot2024/auto/OneAmp.java
+++ src/main/java/com/ragerobotics/robot2024/auto/OneAmp.java
@@ -18,15 +18,15 @@ public class OneAmp extends TaskList {
                 add(new ParallelTask(
                                 new FollowPath(new Path(
                                                 new Pose2d(new Translation2d(0, 0),
-                                                                new Rotation2d(redAlliance ? 0 : Math.PI)),
-                                                new Pose2d(new Translation2d(0.4572, (redAlliance ? 1 : -1) * 0.4572),
-                                                                new Rotation2d(redAlliance ? 0 : Math.PI))),
+                                                                new Rotation2d(redAlliance ? Math.PI : 0)),
+                                                new Pose2d(new Translation2d(0.4572, (redAlliance ? -1 : 1) * 0.4572),
+                                                                new Rotation2d(redAlliance ? Math.PI : 0))),
                                                 true, 5.0),
                                 new TaskList(new Wait(2), new StartDrop(), new Wait(2), new StopDrop())));
-                add(new FollowPath(new Path(new Pose2d(new Translation2d(0.4572, (redAlliance ? 1 : -1) * 0.4572),
+                add(new FollowPath(new Path(new Pose2d(new Translation2d(0.4572, (redAlliance ? -1 : 1) * 0.4572),
                                 new Rotation2d(redAlliance ? 0 : Math.PI)),
-                                new Pose2d(new Translation2d(1.5, (redAlliance ? 1 : -1) * 0.3),
-                                                new Rotation2d(redAlliance ? 0 : Math.PI))),
+                                new Pose2d(new Translation2d(1.5, (redAlliance ? -1 : 1) * 0.3),
+                                                new Rotation2d(redAlliance ? Math.PI : 0))),
                                 true, 5.0));
         }
 }
--- src/main/java/com/ragerobotics/robot2024/auto/TwoAmp.java
+++ src/main/java/com/ragerobotics/robot2024/auto/TwoAmp.java
@@ -18,26 +18,26 @@ public class TwoAmp extends TaskList {
                 add(new ParallelTask(
                                 new FollowPath(new Path(
                                                 new Pose2d(new Translation2d(0, 0),
-                                                                new Rotation2d(redAlliance ? 0 : Math.PI)),
-                                                new Pose2d(new Translation2d(0.4572, (redAlliance ? 1 : -1) * 0.4572),
-                                                                new Rotation2d(redAlliance ? 0 : Math.PI))),
+                                                                new Rotation2d(redAlliance ? Math.PI : 0)),
+                                                new Pose2d(new Translation2d(0.4572, (redAlliance ? -1 : 1) * 0.4572),
+                                                                new Rotation2d(redAlliance ? Math.PI : 0))),
                                                 true, 5.0),
                                 new TaskList(new Wait(2), new StartDrop(), new Wait(2), new StopDrop())));
-                add(new FollowPath(new Path(new Pose2d(new Translation2d(0.4572, (redAlliance ? 1 : -1) * 0.4572),
+                add(new FollowPath(new Path(new Pose2d(new Translation2d(0.4572, (redAlliance ? -1 : 1) * 0.4572),
                                 new Rotation2d(redAlliance ? 0 : Math.PI)),
-                                new Pose2d(new Translation2d(1.5, (redAlliance ? 1 : -1) * 0.3),
-                                                new Rotation2d(redAlliance ? 0 : Math.PI))),
+                                new Pose2d(new Translation2d(1.5, (redAlliance ? -1 : 1) * 0.3),
+                                                new Rotation2d(redAlliance ? Math.PI : 0))),
                                 true, 5.0));
                 add(new StartIntake());
-                add(new FollowPath(new Path(new Pose2d(new Translation2d(1.5, (redAlliance ? 1 : -1) * 0.3),
+                add(new FollowPath(new Path(new Pose2d(new Translation2d(1.5, (redAlliance ? -1 : 1) * 0.3),
                                 new Rotation2d(redAlliance ? 0 : Math.PI)),
                                 new Pose2d(new Translation2d(1.5, 0),
-                                                new Rotation2d(redAlliance ? 0 : Math.PI))),
+                                                new Rotation2d(redAlliance ? Math.PI : 0))),
                                 true, 5.0));
                 add(new ParallelTask(new FollowPath(new Path(new Pose2d(new Translation2d(1.5, 0),
                                 new Rotation2d(redAlliance ? 0 : Math.PI)),
-                                new Pose2d(new Translation2d(0.4572, (redAlliance ? 1 : -1) * 0.4572),
-                                                new Rotation2d(redAlliance ? 0 : Math.PI))),
+                                new Pose2d(new Translation2d(0.4572, (redAlliance ? -1 : 1) * 0.4572),
+                                                new Rotation2d(redAlliance ? Math.PI : 0))),
                                 true, 5.0),
                                 new TaskList(new Wait(2), new StopIntake(), new StartDrop(), new Wait(2),
                                                 new StopDrop())));
