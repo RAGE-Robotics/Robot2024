359b214e34540c8dd698a8d9e7f6d8a578d4c568
Caleb Heydon <caleb@caleb.engineer>
2024-04-04 15:22:20 -0400

    Reverted TwoSpeaker auto

--- src/main/java/com/ragerobotics/robot2024/auto/TwoSpeaker.java
+++ src/main/java/com/ragerobotics/robot2024/auto/TwoSpeaker.java
@@ -8,25 +8,39 @@ import edu.wpi.first.math.geometry.Translation2d;
 public class TwoSpeaker extends TaskList {
         public TwoSpeaker(boolean redAlliance) {
                 add(new FollowPath(new Path(
-                                new Pose2d(new Translation2d(0, 0), new Rotation2d(Math.PI / 2)),
-                                new Pose2d(new Translation2d(0.5, 0), new Rotation2d(Math.PI / 2))),
-                                true, 5.0));
+                                        new Pose2d(new Translation2d(0, 0),
+                                                        new Rotation2d(Math.PI / 2)),
+                                        new Pose2d(new Translation2d(0.5,
+                                                        0),
+                                                        new Rotation2d(Math.PI / 2))),
+                                        true, 5.0));
 
-                add(new SpinShooter());
-                add(new Wait(1));
-                add(new RunShooter());
-                add(new Wait(1));
-                add(new StowShooter());
+                        add(new SpinShooter());
+                        add(new Wait(1));
+                        add(new RunShooter());
+                        add(new Wait(1));
+                        add(new StowShooter());
 
-                add(new TaskList(new StartIntake(), new FollowPath(new Path(
-                                new Pose2d(new Translation2d(0.5, 0), new Rotation2d(Math.PI / 2)),
-                                new Pose2d(new Translation2d(2.0, 0), new Rotation2d(Math.PI / 2))),
-                                true, 5.0), new StopIntake()));
+                        add(new TaskList(new StartIntake(), new FollowPath(new Path(
+                                        new Pose2d(new Translation2d(0.5, 0),
+                                                        new Rotation2d(Math.PI / 2)),
+                                        new Pose2d(new Translation2d(2.0,
+                                                        0),
+                                                        new Rotation2d(Math.PI / 2))),
+                                        true, 5.0), new StopIntake()));
 
-                add(new SpinShooter());
-                add(new Wait(1));
-                add(new RunShooter());
-                add(new Wait(1));
-                add(new StowShooter());
+                        add(new TaskList(new StartIntake(), new FollowPath(new Path(
+                                        new Pose2d(new Translation2d(2.0,
+                                                        0),
+                                                        new Rotation2d(Math.PI / 2)),
+                                        new Pose2d(new Translation2d(0.5, 0),
+                                                        new Rotation2d(Math.PI / 2))),
+                                        true, 5.0), new StopIntake()));
+
+                        add(new SpinShooter());
+                        add(new Wait(1));
+                        add(new RunShooter());
+                        add(new Wait(1));
+                        add(new StowShooter());
         }
 }
